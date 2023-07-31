c04dd9047a65670ba5e57f8321d3f6102359bbdf
Oluwatosin678 <mumunihadiruoluwat@gmail.com>
2023-07-31 05:33:09 -0700

    memset

--- 0-memset.c
+++ 0-memset.c
@@ -1,38 +1,22 @@
 #include "main.h"
 
 /**
-
  * _memset - fill a block of memory with a specific value
-
  * @s: starting address of memory to be filled
-
  * @b: the desired value
-
  * @n: number of bytes to be changed
-
  *
-
  * Return: changed array with new value for n bytes
-
  */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
+	unsigned int i; // Change int to unsigned int for 'i'
 
-        int i = 0;
+	for (i = 0; i < n; i++) // Change i++ to i < n
+	{
+		s[i] = b;
+	}
 
-
-        for (; n > 0; i++)
-
-        {
-
-                s[i] = b;
-
-                n--;
-
-        }
-
-        return (s);
-
+	return (s);
 }
+
